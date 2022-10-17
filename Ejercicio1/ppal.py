df12f7d0052472e7a416861677062bc192f28300
JulianMolina <73005582+Juleeeaaannn@users.noreply.github.com>
2022-10-17 18:44:42 -0300

    Add files via upload

--- Ejercicio1/ppal.py
+++ Ejercicio1/ppal.py
@@ -9,15 +9,33 @@
 # 1. El tamaño de la tabla Hash no es un número primo.
 # 2. El tamaño de la tabla Hash sí es un número primo.
 # Realice un breve análisis comparativo basado en las dos consideraciones anteriores
+from math import trunc
 import random
-from nodo import Nodo
 import numpy as np
 class Hash:
     __tabla=[]
     __dimension=0
-    def __init__(self,dimension=257):
-        self.__dimension=dimension
-        self.__tabla=np.full(dimension,None)
+    def __init__(self,claves):
+        aux=trunc(claves/0.7)
+        self.__dimension=self.primo(aux)
+        print(self.__dimension)
+        self.__tabla=np.full(int(self.__dimension),None)
+
+    def es_primo(self,num):
+        for n in range(2, num):
+            if num % n == 0:
+                return False
+        return True
+    def primo(self,num):
+        band=True
+
+        while band:
+            if( self.es_primo(num) ):
+                band=False
+            else:
+                num+=1
+        return num
+
     # def Insertar(self,elemento):
     #     aux=elemento%self.__dimension #METODO DE DIVISION
     #     if self.__tabla[aux]==None:#pregunto si la cabeza de la lista esta vacia
@@ -28,25 +46,20 @@ class Hash:
     #         while(aux1.getSiguiente()!=None):
     #             aux1=aux1.getSiguiente()
     #         aux1.setSiguiente(Nodo(elemento))
+
     def Insertar(self,elemento):
         aux=elemento%self.__dimension #METODO DE DIVISION
         if self.__tabla[aux]==None:#pregunto si la cabeza de la lista esta vacia
             self.__tabla[aux]=elemento
-            
         else:
-            while(self.__tabla[aux]!=None):
-                if aux<self.__dimension and aux>0:
-                    aux-=1
-                else:
-                    aux=self.__dimension-1
-            if self.__tabla[aux]==None:
+            aux1=aux+1
+            while(self.__tabla[aux]!=None and aux!=aux1):
+                    aux=(aux+1)%self.__dimension
+            if aux!=aux1 and self.__tabla[aux]!=None:
                 self.__tabla[aux]=elemento
             else:
-                print("posicion no econtrada, para insertar el elemento!")
-            
+                print("elemento no econtrada!")
 
-    def Borrar(self):
-        pass
     # def Buscar(self,elemento):
     #     aux=elemento%self.__dimension
     #     if self.__tabla[aux].getElemento()==elemento:
@@ -67,14 +80,11 @@ class Hash:
         if self.__tabla[aux]==elemento:
             retorna=self.__tabla[aux]
         else:
-            while(self.__tabla[aux]!=elemento):
-                if aux<self.__dimension and aux>0:
-                    aux-=1
-                else:
-                    aux=self.__dimension-1
-            if self.__tabla[aux]==elemento:
+            aux1=aux+1
+            while(self.__tabla[aux]!=None and aux!=aux1):
+                    aux=(aux+1)%self.__dimension
+            if aux!=aux1:
                 retorna=self.__tabla[aux]
-                print("posicion",aux)
             else:
                 retorna=None
                 print("elemento no encontrado")
@@ -83,9 +93,10 @@ class Hash:
     def Mostrar(self):
         for i in range(self.__dimension):
             print(i, self.__tabla[i])
+
 if __name__ == '__main__':
-    tabla=Hash()
-    for i in range(200):
-        aux=random.randrange(1000)
+    tabla=Hash(10)
+    for i in range(10):
+        aux=random.randrange(10)
         tabla.Insertar(aux)
     tabla.Mostrar()
